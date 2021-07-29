04638c8999b698744292cd9908e9c96243d9df1d
shareven <1060776877@qq.com>
2021-07-29 12:05:40 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+## Null Safety support
+Fork packages [flutter_aes_ecb_pkcs5](https://pub.dartlang.org/packages/flutter_aes_ecb_pkcs5) 
+
+Solve the null safety support problem
+
+
+## AES 128 pkcs5 only support
+* Provide generation of AES keys
+* Provide encrypt for AES keys  
+* Provide decrypt for AES keys  
+
+* Android uses java, iOS uses OC development
+
+
+## Usage
+
+Use this package as a library
+1. Depend on it
+Add this to your package's pubspec.yaml file:
+
+```
+dependencies:
+  flutter_aes_ecb_pkcs5_fork: ^0.1.1
+```
+
+2. Install it You can install packages from the command line:
+
+with Flutter:
+
+```
+$ flutter pub get
+```
+
+Alternatively, your editor might support flutter pub get. Check the docs for your editor to learn more.
+
+3. Import it Now in your Dart code, you can use:
+
+```
+import 'package:flutter_aes_ecb_pkcs5_fork/flutter_aes_ecb_pkcs5_fork.dart';
+```
+
+
+
+#### AES Use Example
+
+
+
+```dart
+import 'package:flutter_aes_ecb_pkcs5_fork/flutter_aes_ecb_pkcs5_fork.dart';
+
+void main() {
+
+    var data = "{\"username\":\"helloword\"}";
+    
+    //generate a 16-byte random key
+    var key = await FlutterAesEcbPkcs5.generateDesKey(128);
+    
+    print(key);
+    //encrypt
+    var encryptText = await FlutterAesEcbPkcs5.encryptString(data, key);
+    
+    print(encryptText);
+    //decrypt
+    var decryptText  = await FlutterAesEcbPkcs5.decryptString(encryptText, key);
+    
+    print(decryptText);
+  
+}
+```
+
+#### Run Example Result
+```
+data:{"username":"helloword"}
+create key:DA8FD2693B652D587EEE51E7ADD9DBB8
+encryptText :851544E224F6C1C7F560AB82889DE0DF66EEB6762AB836F4EE13E31100F0EA16
+decryptText :{"username":"helloword"}
+```
+
+#### License
+
+The MIT License(http://opensource.org/licenses/MIT)
+
+Please feel free to use and contribute to the development.
+
+
---
+++ example/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter_aes_ecb_pkcs5_fork/flutter_aes_ecb_pkcs5_fork.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String pkcs5Progress;
+
+    //string format
+    var data = "{\"username\":\"helloword\"}";
+
+    //create 16 byte random key
+    var key = await FlutterAesEcbPkcs5.generateDesKey(128);
+
+    print(key);
+    //encrypt
+    var encryptText = await FlutterAesEcbPkcs5.encryptString(data, key);
+
+    print(encryptText);
+    //decrypt
+    var decryptText  = await FlutterAesEcbPkcs5.decryptString(encryptText, key);
+
+    print(decryptText);
+
+    pkcs5Progress = "data:"+data +"\n"+
+        "create key:"+key +"\n"
+        +"encryptText :"+encryptText +"\n"
+        +"decryptText :"+decryptText +"\n";
+
+    print(pkcs5Progress);
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = pkcs5Progress;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('flutter_aes_ecb_pkcs5'),
+        ),
+        body: Center(
+          child: Text('pkcs5Progress:\n $_platformVersion\n'),
+        ),
+      ),
+    );
+  }
+}
